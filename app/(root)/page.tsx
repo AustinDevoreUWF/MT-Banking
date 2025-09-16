979498742cb58493160b480929be6f679a8bfd32
austinwatspoppin <austind2005@gmail.com>
2025-09-16 06:52:09 -0500

    balance header added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 const Home = () => {
   const loggedIn={firstName:'Austin'};
+  
   return (
     <section className="home">
         <div className="home-content">
@@ -13,7 +15,12 @@ const Home = () => {
                 subtext="Access and manage your account and transactions efficiantly"
                 />
             </header>
+            <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}/>
         </div>
+
     </section>
   )
 }
