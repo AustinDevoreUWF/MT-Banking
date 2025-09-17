7c8ddab7e166435ad9174e18aff15eb4d87bca15
austinwatspoppin <austind2005@gmail.com>
2025-09-16 23:04:46 -0500

    Added SideBar component to home

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, plugins } from 'chart.js'
 import {Doughnut} from 'react-chartjs-2';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
@@ -11,13 +11,21 @@ const data = {
         {
             label:'Banks',
             data: [1250,2500,3750],
-            background:['#0747b6', '#2265d8','#2f91fa'],
+            backgroundColor:['#0747b6', '#2265d8','#2f91fa'],
         }
     ],
     labels:['Bank 1', 'Bank 2', 'Bank 3']
 }
     return (
-    <Doughnut data={data} />
+    <Doughnut data={data}
+    options={{cutout:'60%',
+            plugins:{
+            legend:{
+                display:false
+                    }
+                    }
+            }}
+/>
   )
 }
 
